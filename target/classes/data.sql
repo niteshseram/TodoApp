bbd4324abd1237a37c85b6741abf48d00a449714
Nitesh Seram <niteshseram@gmail.com>
2021-02-19 19:28:34 +0530

    Delete target/classes directory

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-insert into TODO
-values (10001, 'Learn Spring Boot',false, sysdate(), 'test');
-insert into TODO
-values (10002, 'Learn Spring MVC',false, sysdate(), 'test');
-insert into TODO
-values (10003, 'Learn AngularJS',false, sysdate(), 'test');
-insert into TODO
-values (10004, 'Learn to play Guitar',false, sysdate(), 'test');
